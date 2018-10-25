262bef150a33a8fdecc8db8962f1923b2e54a956
Junzhe Zhang <zhangjunzhe8868@gmail.com>
2018-10-25 12:12:31 -0700

    fix2

--- src/h1b.py
+++ src/h1b.py
@@ -14,7 +14,7 @@ job_arr=[]
 state_per1=[]
 job_per1=[]
 #with open ('H1B_FY_2016.csv',newline='',encoding="utf8") as csvfile:
-with open ('H1B_FY_2016.csv') as csvfile:
+with open ('./src/H1B_FY_2016.csv') as csvfile:
     reader=csv.reader(csvfile,delimiter=';',quotechar='|')
     row1=next(reader)
     for i in range (len(row1)):
