8f60d58f899b056ac4b3f4b853f64fd332b4324c
Junzhe Zhang <zhangjunzhe8868@gmail.com>
2018-10-27 20:06:22 -0700

    test

--- src/h1b_counting.py
+++ src/h1b_counting.py
@@ -14,9 +14,11 @@ job_arr=[]
 state_per1=[]
 job_per1=[]
 
+##read csv
 with open ('./input/h1b_input.csv') as csvfile:
     reader=csv.reader(csvfile,delimiter=';',quotechar='|')
     row1=next(reader)
+	##find the titles of job and state 
     for i in range (len(row1)):
         if row1[i].find('SOC_NAME')!=-1:
             job_title=i
@@ -31,10 +33,12 @@ with open ('./input/h1b_input.csv') as csvfile:
             job.extend([row[job_title]])
         else:
             count2+=1
-            
+
+##find the top ten most job and state            
 state_list=Counter(state).most_common(10)
 job_list=Counter(job).most_common(10)
 
+##build lists of str and arr
 for i in range (len(state_list)):
     state_str.append(state_list[i][0])
     state_arr.append(state_list[i][1])   
@@ -45,9 +49,11 @@ for i in range (len(job_list)):
     job_arr.append(job_list[i][1])   
 job_per=list(map(lambda i:i/count1,job_arr))
 
+##delete '\"'
 for i in range (len(job_list)):
     job_str[i]=re.sub("\"", "",job_str[i])
 
+##build list of percentage
 for i in range(len(state_list)):
     temp=format(state_per[i],'3.1%')
     state_per1.append(temp)
@@ -56,23 +62,25 @@ for i in range(len(job_list)):
     temp=format(job_per[i],'3.1%')
     job_per1.append(temp)
 
+##combine the lists of str, arr and percentage
 state_output=[state_str,state_arr,state_per1]
 state_output1=list(map(list, zip(*state_output)))
     
 job_output=[job_str,job_arr,job_per1]
 job_output1=list(map(list, zip(*job_output)))
 
+##sort the sequences of str, arr and percentage  
 state_output2=sorted(state_output1,key=lambda state_output1:(-state_output1[1],state_output1[0]))
 job_output2=sorted(job_output1,key=lambda job_output1:(-job_output1[1],job_output1[0]))
 
+##output the result
 output = open('./output/top_10_occupations.txt','w')
 output.write('TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
 for row in job_output2:
     rowtxt = '{};{};{}'.format(row[0],row[1],row[2])
     output.write(rowtxt)
     output.write('\n')
-output.close()
-            
+output.close()          
 
 output = open('./output/top_10_states.txt','w')
 output.write('TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE\n')
